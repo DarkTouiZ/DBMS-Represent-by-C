8a2264c627226fed118f154d206172a56d511304
Adisorn Parama <adisorn.safe01@gmail.com>
2024-05-06 20:47:10 +0700

    Row and Column structure
    ตอนใช้ก็
    #include "lib\Structure.c"

---
+++ lib/Structure.h
@@ -0,0 +1,30 @@
+typedef enum { false, true } bool;
+
+typedef struct Linked Adjacent_Node;
+typedef struct Tree Tree;
+
+typedef struct Table_Head
+{
+    bool Type; // false/true :: This Column contain with String/Integer
+    bool isIndexing; // false/true :: This Column has been Indexing(By B-Tree)
+    Tree* Indexing_Ptr; // Ptr to B-Tree
+    char Data[100]; // Name of Column Header
+    struct Table_Head* Next; // Ptr to Next Column
+    struct Table_Head* Prev; // Ptr to Prev Column
+    Adjacent_Node* Head; // Ptr to first Row through this Column
+    Adjacent_Node* Tail; // Ptr to last Row through this Column
+}Node;
+
+typedef struct Linked{
+    char Data[100]; // Data in Row
+    struct Linked* Next; // Next Row
+    struct Linked* Prev; // Prev Row
+} Adjacent_Node;
+
+typedef struct Tree{
+    char Key[100]; // Key used for sorted in the Tree
+    int Height;
+    Node* Ptr; // Ptr to actual data in Database
+    struct Tree* Left; // LeftChild
+    struct Tree* Right; // RightChild
+}Tree;
